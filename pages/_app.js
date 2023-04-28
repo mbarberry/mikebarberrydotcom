1354eb9de49352604b6834d299d7ac57877e0817
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2023-04-27 21:01:13 -0700

    add title to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import 'normalize.css/normalize.css';
+import Head from 'next/head';
 
 import theme from '../styles/theme';
 import Layout from '../components/Layout';
@@ -7,10 +7,14 @@ import Layout from '../components/Layout';
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
+import 'normalize.css/normalize.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>MikeBarberry.com</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>

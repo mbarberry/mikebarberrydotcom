1d051bc1aa0ada2928ab5f94e0c5dca12c85ac0f
Mike Barberry <52507419+MikeBarberry@users.noreply.github.com>
2023-10-08 20:12:18 -0700

    center loading spinner on mobile

--- frontend/components/index/Loading.js
+++ frontend/components/index/Loading.js
@@ -1,13 +1,17 @@
 import { chakra, Spinner } from '@chakra-ui/react';
+import { useContext } from 'react';
+import { MobileContext } from '../context/MobileContext';
 
 export default function Loading({ loading }) {
+  const mobile = useContext(MobileContext);
+
   return (
     loading && (
       <chakra.div justify='center'>
         <Spinner
           pos='absolute'
           top='40%'
-          left='50%'
+          left={mobile ? '45%' : '50%'}
           thickness='4px'
           speed='0.65s'
           emptyColor='gray.100'
